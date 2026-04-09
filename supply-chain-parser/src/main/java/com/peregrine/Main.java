fe44e242e9c55721fbc9565316149f3ac75d603f
owl-18 <twoblibion18@gmail.com>
2026-04-09 18:55:58 +0700

    feat(facade): add BarcodeScanner facade and wire up Main with user input

--- supply-chain-parser/src/main/java/com/peregrine/Main.java
+++ supply-chain-parser/src/main/java/com/peregrine/Main.java
@@ -1,26 +1,25 @@
 package com.peregrine;
 
-import com.peregrine.model.Barcode;
-import com.peregrine.model.BarcodeResponse;
+import com.peregrine.facade.BarcodeScanner;
 import com.peregrine.processor.BarcodeProcessor;
 import com.peregrine.service.GS1PrefixService;
 import com.peregrine.validator.BarcodeValidator;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    Barcode barcodeFake = new Barcode("8931234567890");
-    Barcode barcodeAuth = new Barcode("4006381333931");
-    BarcodeValidator barcodeValidator = new BarcodeValidator();
-    GS1PrefixService gs1PrefixService = new GS1PrefixService();
-    BarcodeProcessor barcodeProcessor = new BarcodeProcessor(barcodeValidator, gs1PrefixService);
-    BarcodeResponse response = barcodeProcessor.process(barcodeFake);
-    System.out.println("Barcode: " + barcodeFake.getRawValue());
-    System.out.println("Authentication: " + response.isValid());
-    System.out.println("Country: " + response.getCountry());
+    BarcodeValidator validator = new BarcodeValidator();
+    GS1PrefixService service = new GS1PrefixService();
 
-    BarcodeResponse response2 = barcodeProcessor.process(barcodeAuth);
-    System.out.println("Barcode: " + barcodeAuth.getRawValue());
-    System.out.println("Authentication: " + response2.isValid());
-    System.out.println("Country: " + response2.getCountry());
+    BarcodeProcessor processor = new BarcodeProcessor(validator, service);
+
+    BarcodeScanner scannerFacade = new BarcodeScanner(processor);
+
+    try (Scanner inputReader = new Scanner(System.in)) {
+      String result = scannerFacade.scan(inputReader.nextLine());
+      System.out.println(result);
+    } catch (Exception e) {
+      System.out.println("An error occurred: " + e.getMessage());
+    }
   }
 }
---
+++ supply-chain-parser/src/main/java/com/peregrine/facade/BarcodeScanner.java
@@ -0,0 +1,22 @@
+package com.peregrine.facade;
+
+import com.peregrine.model.Barcode;
+import com.peregrine.model.BarcodeResponse;
+import com.peregrine.processor.BarcodeProcessor;
+
+public class BarcodeScanner {
+  private final BarcodeProcessor barcodeProcessor;
+
+  public BarcodeScanner(BarcodeProcessor barcodeProcessor) {
+    this.barcodeProcessor = barcodeProcessor;
+  }
+
+  public String scan(String userInput) {
+    Barcode barcode = new Barcode(userInput);
+    BarcodeResponse barcodeResponse = barcodeProcessor.process(barcode);
+
+    return String.format(
+        "Barcode: %s\nAuthentication: %s\nCountry: %s",
+        barcode.getRawValue(), barcodeResponse.isValid(), barcodeResponse.getCountry());
+  }
+}
