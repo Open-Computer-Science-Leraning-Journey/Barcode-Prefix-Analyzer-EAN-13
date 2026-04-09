781a1ee742b2414bce9ea07810d1dccd477d677e
owl-18 <twoblibion18@gmail.com>
2026-04-09 23:32:15 +0700

    refactor(response): remove field isValid

--- src/main/java/com/peregrine/model/BarcodeResponse.java
+++ src/main/java/com/peregrine/model/BarcodeResponse.java
@@ -2,18 +2,12 @@ package com.peregrine.model;
 
 public class BarcodeResponse {
   private final String country;
-  private final boolean isValid;
 
   public BarcodeResponse(String country, boolean isValid) {
     this.country = country;
-    this.isValid = isValid;
   }
 
   public String getCountry() {
     return this.country;
-  }
-
-  public boolean isValid() {
-    return this.isValid;
   }
 }
